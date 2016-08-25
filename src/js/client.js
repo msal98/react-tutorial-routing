291afd1f414211c1f78ab1e731901364b954b99a
Matthew Salvatore <matt.salvatore@echoman.com>
2016-08-25 19:02:46 +0000

    Add simple routing support

--- src/js/client.js
+++ src/js/client.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+
 import Layout from "./components/Layout";
 
-var PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
-
 const app = document.getElementById('app');
-ReactDOM.render(<Layout products={PRODUCTS} />, app);
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+    </Route>
+  </Router>,
+  app);
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,6 +4,15 @@ import Footer from "./Footer";
 import FilterableProductTable from "./FilterableProductTable";
 import Header from "./Header";
 
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
 export default class Layout extends React.Component {
 
   constructor() {
@@ -21,7 +30,7 @@ export default class Layout extends React.Component {
     return (
       <div>
         <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
-        <FilterableProductTable products={this.props.products}/>
+        <FilterableProductTable products={PRODUCTS}/>
         <Footer />
       </div>
     )
