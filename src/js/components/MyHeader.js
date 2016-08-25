74ad8d85525919ee51d26c6d37b89702222e13c1
Matthew Salvatore <matt.salvatore@echoman.com>
2016-08-25 17:15:06 +0000

    More tutorial changes

--- src/js/components/MyHeader.js
+++ src/js/components/MyHeader.js
@@ -3,10 +3,17 @@ import React from "react";
 import MyTitle from "./MyHeader/MyTitle";
 
 export default class MyHeader extends React.Component {
+
+  handleChange(e) {
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
+
   render() {
     return (
       <div>
-        <MyTitle />
+        <MyTitle title={this.props.title}/>
+        <input value={this.props.title} onChange={this.handleChange.bind(this)}/>
       </div>
     )
   }
--- src/js/components/MyLayout.js
+++ src/js/components/MyLayout.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 import MyFooter from "./MyFooter";
+import FilterableProductTable from "./FilterableProductTable";
 import MyHeader from "./MyHeader";
 
 export default class MyLayout extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      title: "Welcome",
+    };
+  }
+
+  changeTitle(title) {
+    this.setState({title})
+  }
+
   render() {
     return (
       <div>
-        <MyHeader />
+        <MyHeader changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
+        <FilterableProductTable products={this.props.products}/>
         <MyFooter />
       </div>
     )
